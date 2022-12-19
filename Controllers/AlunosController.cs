4022e38eac6592a3681e3f7a2164b963bfee705a
hvictorleite <hvictorleite@gmail.com>
2022-12-19 13:20:30 -0300

    Criado AlunosController com métodos 'GetAlunos()' e 'GetAlunosByNome'.

---
+++ Controllers/AlunosController.cs
@@ -0,0 +1,58 @@
+﻿using AlunosAPI.Models;
+using AlunosAPI.Services;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AlunosAPI.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    //[Produces("application/json")]
+    public class AlunosController : ControllerBase
+    {
+        private IAlunoService _alunoService;
+
+        public AlunosController(IAlunoService alunoService)
+        {
+            _alunoService = alunoService;
+        }
+
+        [HttpGet]
+        /*[ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]*/
+        public async Task<ActionResult<IAsyncEnumerable<Aluno>>> GetAlunos()
+        {
+            try
+            {
+                var alunos = await _alunoService.GetAlunos();
+                return Ok(alunos);
+            }
+            catch
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, "Erro ao obter alunos");
+            }
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Aluno>>> GetAlunosByNome([FromQuery] string nome)
+        {
+            try
+            {
+                var alunos = await _alunoService.GetAlunosByNome(nome);
+                
+                if (alunos.Count() == 0)
+                    return NotFound($"Não existem alunos com o critério '{nome}'");
+
+                return Ok(alunos);
+            }
+            catch
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, "Erro ao obter alunos");
+            }
+        }
+    }
+}
