a00703e8d88a383b52669a99bb711a83cc955b83
hvictorleite <hvictorleite@gmail.com>
2022-12-19 10:03:05 -0300

    Instalados pacotes de banco de dados e estabelecida conexão.

---
+++ Context/AppDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace AlunosAPI.Context
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> opts): base(opts)
+        {
+
+        }
+    }
+}
