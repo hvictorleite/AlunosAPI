8798eb45ac345aabb4d7025cba230736b35727d6
hvictorleite <hvictorleite@gmail.com>
2022-12-19 10:20:41 -0300

    Criado model Aluno, modelao e populado no BD com EF e migrations.

--- Context/AppDbContext.cs
+++ Context/AppDbContext.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using AlunosAPI.Models;
+using Microsoft.EntityFrameworkCore;
 
 namespace AlunosAPI.Context
 {
@@ -6,7 +7,28 @@ namespace AlunosAPI.Context
     {
         public AppDbContext(DbContextOptions<AppDbContext> opts): base(opts)
         {
+        }
 
+        public DbSet<Aluno> Alunos { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Aluno>().HasData(
+                new Aluno
+                {
+                    Id = 1,
+                    Nome = "Maria da Pena",
+                    Email = "mariapena@yahoo.com",
+                    Idade = 23
+                },
+                new Aluno
+                {
+                    Id = 2,
+                    Nome = "Manuel Bueno",
+                    Email = "manuelbueno@yahoo.com",
+                    Idade = 22
+                }
+            );
         }
     }
 }
