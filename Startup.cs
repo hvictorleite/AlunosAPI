4d7f2db2a2808e5231bf77bdc9f94f86fafe1174
hvictorleite <hvictorleite@gmail.com>
2022-12-20 11:53:02 -0300

    Adicionada Policy para permitir o acesso do axios da aplicação React a API.

--- Startup.cs
+++ Startup.cs
@@ -34,6 +34,16 @@ namespace AlunosAPI
 
             services.AddScoped<IAlunoService, AlunoService>();
 
+            services.AddCors(options =>
+            {
+                options.AddPolicy(name: "AlunosReact", builder =>
+                {
+                    builder.WithOrigins("http://localhost:3000")
+                        .AllowAnyHeader()
+                        .AllowAnyMethod();
+                });
+            });
+
             services.AddControllers();
             services.AddSwaggerGen(c =>
             {
@@ -50,6 +60,8 @@ namespace AlunosAPI
                 app.UseSwagger();
                 app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "AlunosAPI v1"));
             }
+
+            app.UseCors("AlunosReact");
 
             app.UseHttpsRedirection();
 
