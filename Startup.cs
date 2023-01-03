a5254af055088dc234536bffa43d2c4c1d288846
hvictorleite <hvictorleite@gmail.com>
2023-01-03 09:42:50 -0300

    Habilitada autenticação por JWT Bearer Token através do Swagger. Utilize: "Bearer [token]"

--- Startup.cs
+++ Startup.cs
@@ -72,6 +72,31 @@ namespace AlunosAPI
             services.AddSwaggerGen(c =>
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "AlunosAPI", Version = "v1" });
+
+                // Habilitar autorização através do Swagger (JWT)
+                c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme()
+                {
+                    Name = "Authorization",
+                    Type = SecuritySchemeType.ApiKey,
+                    Scheme = "Bearer",
+                    BearerFormat = "JWT",
+                    In = ParameterLocation.Header,
+                    Description = "JWT Authorization header using the Bearer scheme." +
+                    " \r\n\r\n Enter 'Bearer' [space] and then your token in the text input below." +
+                    " \r\n\r\n Example: \"Bearer 123545abcdef\""
+                });
+                c.AddSecurityRequirement(new OpenApiSecurityRequirement
+                {
+                    {
+                        new OpenApiSecurityScheme
+                        {
+                            Reference = new OpenApiReference
+                            {
+                                Type = ReferenceType.SecurityScheme,
+                                Id = "Bearer"
+                            }
+                        }, new string[] { } }
+                });
             });
         }
 
