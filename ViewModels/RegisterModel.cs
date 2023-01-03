688d8209ddf02de7bcd9b6e612e33d5b93b879d4
hvictorleite <hvictorleite@gmail.com>
2023-01-03 08:36:15 -0300

    Criados ViewModels para autenticação.

---
+++ ViewModels/RegisterModel.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace AlunosAPI.ViewModels
+{
+    public class RegisterModel
+    {
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+
+        [DataType(DataType.Password)]
+        [Display(Name = "Confirmar Senha")]
+        [Compare("Password", ErrorMessage = "Senhas não coincidem")]
+        public string ConfirmPassword { get; set; }
+    }
+}
